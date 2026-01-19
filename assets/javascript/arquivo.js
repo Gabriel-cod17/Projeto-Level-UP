a2756e4f0cf2dfd18e894689593f990c7442e3b6
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-01-19 16:03:34 -0300

    menu mobile parte 1

--- assets/javascript/arquivo.js
+++ assets/javascript/arquivo.js
@@ -5,6 +5,23 @@ const saiu_Cima = document.querySelectorAll(".saiu-cima");
 const saiu_lado_esquerdo = document.querySelectorAll(".saiu-lado-esquerdo");
 const saiu_lado_direito = document.querySelectorAll(".saiu-lado-direito");
 const saiu_baixo = document.querySelectorAll(".saiu-baixo ");
+const divAbrirMenu = document.getElementById("div_abrir_Menu");
+const headerMobile = document.querySelector(".header_Mobile");
+const link = document.querySelectorAll(".link");
+
+link.forEach((l) => {
+  l.addEventListener("click", () => {
+    headerMobile.style.display = "none";
+  });
+});
+
+divAbrirMenu.addEventListener("click", () => {
+  if (headerMobile.style.display == "flex") {
+    headerMobile.style.display = "none";
+  } else {
+    headerMobile.style.display = "flex";
+  }
+});
 
 const observerCima = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
