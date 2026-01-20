700983d19475a480e125f3024b14c47d04b90eba
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-01-20 16:05:24 -0300

    menu mobile parte 2 + parte política

--- assets/javascript/arquivo.js
+++ assets/javascript/arquivo.js
@@ -4,22 +4,31 @@ const header_Agencia_Marketing = document.getElementById(
 const saiu_Cima = document.querySelectorAll(".saiu-cima");
 const saiu_lado_esquerdo = document.querySelectorAll(".saiu-lado-esquerdo");
 const saiu_lado_direito = document.querySelectorAll(".saiu-lado-direito");
-const saiu_baixo = document.querySelectorAll(".saiu-baixo ");
+const saiu_baixo = document.querySelectorAll(".saiu-baixo");
 const divAbrirMenu = document.getElementById("div_abrir_Menu");
 const headerMobile = document.querySelector(".header_Mobile");
 const link = document.querySelectorAll(".link");
+const img_abrir_Menu = document.getElementById("img_abrir_Menu");
 
 link.forEach((l) => {
   l.addEventListener("click", () => {
-    headerMobile.style.display = "none";
+    if (headerMobile.classList.contains("voltou")) {
+      headerMobile.classList.remove("voltou");
+      img_abrir_Menu.src = "assets/img/menu-aberto.png";
+    } else {
+      headerMobile.classList.add("voltou");
+      img_abrir_Menu.src = "assets/img/menu-fechado.png";
+    }
   });
 });
 
 divAbrirMenu.addEventListener("click", () => {
-  if (headerMobile.style.display == "flex") {
-    headerMobile.style.display = "none";
+  if (headerMobile.classList.contains("voltou")) {
+    headerMobile.classList.remove("voltou");
+    img_abrir_Menu.src = "assets/img/menu-aberto.png";
   } else {
-    headerMobile.style.display = "flex";
+    headerMobile.classList.add("voltou");
+    img_abrir_Menu.src = "assets/img/menu-fechado.png";
   }
 });
 
