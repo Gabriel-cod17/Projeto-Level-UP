1bdecf1c7b3bccf914ceed5f4fdfa34c4ab3d8e6
Gabriel Felipe <gabrielssantiago1007@gmail.com>
2026-01-26 11:16:46 -0300

    Adicionando comentários no arquivo.js

--- assets/javascript/arquivo.js
+++ assets/javascript/arquivo.js
@@ -11,6 +11,8 @@ const link = document.querySelectorAll(".link");
 const img_abrir_Menu = document.getElementById("img_abrir_Menu");
 const video = document.getElementById("video");
 
+// Função para fazer com que o video faça o autoplay quando clicar na tela
+
 const startVideo = () => {
   video.muted = true;
   video.play().catch(() => {});
@@ -18,6 +20,8 @@ const startVideo = () => {
 };
 
 document.addEventListener("touchstart", startVideo);
+
+// Função para que cada link do menu mobile ao ser clicado ele fecha o menu e troque a imagem do menu mobile
 link.forEach((l) => {
   l.addEventListener("click", () => {
     if (headerMobile.classList.contains("voltou")) {
@@ -30,6 +34,8 @@ link.forEach((l) => {
   });
 });
 
+// Evento para quando clicar no menu mobile ele adiciona uma classe com efeito responsivo e troque as imagens do menu
+
 divAbrirMenu.addEventListener("click", () => {
   if (headerMobile.classList.contains("voltou")) {
     headerMobile.classList.remove("voltou");
@@ -40,6 +46,8 @@ divAbrirMenu.addEventListener("click", () => {
   }
 });
 
+// função para que o quando a página entra, os efeitos css da div saindo de cima funcione
+
 const observerCima = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -47,6 +55,8 @@ const observerCima = new IntersectionObserver((entries) => {
     }
   });
 });
+
+// função para que o quando a página entra, os efeitos css da div saindo de esquerda funcione
 
 const observeEsquerdo = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -56,21 +66,29 @@ const observeEsquerdo = new IntersectionObserver((entries) => {
   });
 });
 
+// Para todas as divs que tiverem a classe saiu de cima ele faz efeito para cada uma
+
 saiu_Cima.forEach((s) => {
   observerCima.observe(s);
 });
+
+// Para todas as divs que tiverem a classe saiu de baixo ele faz efeito para cada uma
 
 saiu_baixo.forEach((s) => {
   observerCima.observe(s);
 });
 
+// Para todas as divs que tiverem a classe saiu lado esquerdo ele faz efeito para cada uma
 saiu_lado_esquerdo.forEach((s) => {
   observeEsquerdo.observe(s);
 });
 
+// Para todas as divs que tiverem a classe saiu lado direito ele faz efeito para cada uma
 saiu_lado_direito.forEach((s) => {
   observeEsquerdo.observe(s);
 });
+
+// função de rolagem da página, se rolar  mais de 50px ele adiciona a classe scrolled, se não ele remove a classe scrolled
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
